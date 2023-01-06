5ee48c2f692f252be29a85e293dfbef671849064
Yasser Shalash <yasservanshalash@gmail.com>
2023-01-06 14:45:49 +0100

    added sorting to category pages

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -7,21 +7,27 @@ import "./Categories.css"
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import ProductItem from '../components/Products/ProductItem';
+import SortBar from '../components/SortBar/SortBar';
 const Categories = () => {
     const [items, setItems]  = useState<Product[]>([]);
     let { name } = useParams();
     const products = useSelector((state: RootState) => state.products.products)
 
   return (
-    <>
-        {   products.length === 0 ? <h1>Loading</h1> :
+    <div className='productsPage'>
+    <div>
+    <SortBar />
+    </div>
+    <div>
+    {   products.length === 0 ? <h1>Loading</h1> :
             products.filter((product) => product.category === name).map((product) => {
                 return (
                     <ProductItem key={crypto.randomUUID()} product={product} />
                 )
             })
         }
-    </>
+    </div>
+    </div>
     )
 }
 
